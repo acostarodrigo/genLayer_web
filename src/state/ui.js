1012986971486b81e94736e1202b05b65c4445c0
Rodrigo Acosta <acosta_rodrigo@hotmail.com>
2024-08-16 17:11:12 -0400

    added global backdrop

--- src/state/ui.js
+++ src/state/ui.js
@@ -4,19 +4,27 @@ export const uiSlice = createSlice({
   name: "ui",
   initialState: {
     snackbar: { show: false, message: "", severity: "success" },
+    showBackdrop: false,
   },
   reducers: {
-    showSnackbar: (state, payload) => {
-      state.snackbar.message = payload.message;
-      state.snackbar.severity = payload.severity;
+    showSnackbar: (state, action) => {
+      state.snackbar.message = action.payload.message;
+      state.snackbar.severity = action.payload.severity;
       state.snackbar.show = true;
     },
     hideSnackbar: (state) => {
       state.snackbar = { show: false };
     },
+    showBackdrop: (state) => {
+      state.showBackdrop = true;
+    },
+    hideBackdrop: (state) => {
+      state.showBackdrop = false;
+    },
   },
 });
 
-export const { showSnackbar, hideSnackbar } = uiSlice.actions;
+export const { showSnackbar, hideSnackbar, showBackdrop, hideBackdrop } =
+  uiSlice.actions;
 
 export default uiSlice.reducer;
